1e6d331c2a2cf200d439e195fec308d729ce3d27
perepeli <systemofadowng@gmail.com>
2025-03-21 15:32:09 -0400

    schema

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=200)
+    no_of_guests = models.IntegerField()
+    booking_date= models.DateTimeField()
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.IntegerField()
+    inventory = models.IntegerField()
+    
+    def __str__(self):
+        return f'{self.title} : {self.price}'
